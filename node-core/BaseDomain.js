9870d6e75b619dda5add2d51e80da88cfd20a8f4
Max <sibusiso@5ivedesign.co.za>
2016-06-01 15:31:48 +0200

    Fixed Adding Equipment String/Number Date Error

--- node-core/BaseDomain.js
+++ node-core/BaseDomain.js
@@ -92,16 +92,16 @@
 
 		var DBconfigObj = fs.readJsonSync( path.join( __dirname, '../', "config.json" ) );
 
-		//svc.user.domain 	= DBconfigObj.production.sudoDomain;//os.hostname();
-		//svc.user.account 	= DBconfigObj.production.sudoUser;
-		//svc.user.password 	= DBconfigObj.production.sudoPwd;
+		//svc.user.domain 	= DBconfigObj.development.sudoDomain;//os.hostname();
+		//svc.user.account 	= DBconfigObj.development.sudoUser;
+		//svc.user.password 	= DBconfigObj.development.sudoPwd;
 
 		_SQL_db_config 	= {
 			//socketPath 		: address.port,
-			host     			: DBconfigObj.production.host,
-			user     			: DBconfigObj.production.user,
-			password 			: DBconfigObj.production.password,
-			database 			: DBconfigObj.production.database,
+			host     			: DBconfigObj.development.host,
+			user     			: DBconfigObj.development.user,
+			password 			: DBconfigObj.development.password,
+			database 			: DBconfigObj.development.database,
 			debug 				: false,
 			multipleStatements 	: true,
 			insecureAuth 		: true
